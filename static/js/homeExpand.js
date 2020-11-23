2f2e3c315f8dbe8b76fc64635f32f43bc900bbf5
Scott Cottam <scott@scottcottam.co.uk>
2020-11-23 20:43:59 +0000

    JS with CSS

---
+++ static/js/homeExpand.js
@@ -0,0 +1,44 @@
+const productButton = document.querySelector('.expandProducts')
+const articleButton = document.querySelector('.expandArticles')
+const mediaQuery = window.matchMedia('(max-width: 601px)')
+
+const articles = document.querySelector('.latestPosts')
+const products = document.querySelector('.latestProducts')
+
+function expandProducts() {
+
+    if(products.style.display !== "none"){
+        products.style.display = "none"
+        productButton.getElementsByTagName('i')[0].className = "fas fa-angle-down"
+    } else {
+        products.style.display = "grid"
+        productButton.getElementsByTagName('i')[0].className = "fas fa-angle-up"
+    }
+}
+
+function expandArticles() {
+
+        if(articles.style.display !== "none"){
+            articles.style.display = "none"
+            articleButton.getElementsByTagName('i')[0].className = "fas fa-angle-down"
+        } else {
+            articles.style.display = "grid"
+            articleButton.getElementsByTagName('i')[0].className = "fas fa-angle-up"
+        }
+}
+
+productButton.addEventListener("click", expandProducts)
+articleButton.addEventListener("click", expandArticles)
+
+mediaQuery.addEventListener("change", () => {
+    if(mediaQuery.matches === false){
+        products.style.display = "grid"
+        articles.style.display = "grid"
+    } else {
+        products.style.display = "none"
+        productButton.getElementsByTagName('i')[0].className = "fas fa-angle-down"
+        articles.style.display = "none"
+        articleButton.getElementsByTagName('i')[0].className = "fas fa-angle-down"
+    }
+});
+
